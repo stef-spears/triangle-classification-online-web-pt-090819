43e45985a86ec044d33011de2aa2a7535a0d9131
Stef Spears <spears.sj87@gmail.com>
2019-10-14 06:31:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,27 @@
 class Triangle
   
-  attr_reader :side_1, :side_2, :side_3
+  attr_reader :side_1, :side_2, :side_3, :lengths
   
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @lengths = [side_3, side_2, side_1]
   end
   
   def kind
     
-    
     # put sides in an array, iterate thru
+    
+    @lengths.each do |side|
+      if side < 0
+        begin
+        raise TriangleError
+        rescue TriangleError => error 
+        puts error.message
+      end
+    end
+    
     # if any side < 0
     # throw error 
     # elsif all 3 sides have same length
@@ -24,8 +34,11 @@ class Triangle
     #
     # scalene
   
-    #class TriangleError < StandardError
-    #end
+    class TriangleError < StandardError
+      def message
+        "you done goofed"
+      end
+    end
     
   end
   
