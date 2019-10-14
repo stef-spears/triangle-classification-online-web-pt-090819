fa7171795ca3be89458b73f35d74fa195463037e
Stef Spears <spears.sj87@gmail.com>
2019-10-14 07:04:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def kind
     
     # if any side < 0 or any two sides are less than the third side raise error
-      if (@side_1 + @side_2 < @side_3) || (@side_2 + @side_3 < @side_1) || (@side_1 + @side_3 < @side_2) || @sides.any
+      if (@side_1 + @side_2 < @side_3) || (@side_2 + @side_3 < @side_1) || (@side_1 + @side_3 < @side_2) || @sides.any? {|n| n <= 0}
         #begin
         raise TriangleError
         #rescue TriangleError => error
