ff2d02e0f43b00690555dc1c79d05814a5c84142
Stef Spears <spears.sj87@gmail.com>
2019-10-14 06:17:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+    
+    
+    class TriangleError < StandardError
+    end
+    
+  end
+  
+  
+  
 end
